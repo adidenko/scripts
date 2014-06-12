6ddb9b946948a093cea5b601d09ba16b2090a84b
Aleksandr Didenko <adidenko@mirantis.com>
2014-06-12 13:01:01 +0300

    Added fuel_test_puppet.sh
    Script iterates over predefined astute.yaml examples and tests
    manifests. Created for:
    
    https://etherpad.openstack.org/p/fuel_puppet_modules_upgrade

---
+++ fuel_test_puppet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+YAML_URL_BASE="https://github.com/adidenko/scripts/blob/master/fuel-yaml/5.1/"
+YAMLS="
+ceph-osd.node-2.test.domain.local.yaml
+cinder.node-5.domain.tld.yaml
+compute.node-4.domain.tld.yaml
+controller.ceph.node-3.test.domain.local.yaml
+controller.node-2.domain.tld.yaml
+primary-controller.node-1.domain.tld.yaml
+"
+
+printf "%-60s%-10s %s\n" "YAML EXAMPLE" "RESULT" "LOGFILE"
+for YAML in $YAMLS ; do
+    printf "%-60s" "Running $YAML ..."
+    curl -s "$YAML_URL_BASE/$YAML" > /etc/astute.yaml
+    ( FACTER_hostname=`awk '/^fqdn:/{print $2}' /etc/fuel/astute.yaml | cut -d. -f1` puppet apply -d -v --modulepath /tmp/modules/upstream:/tmp/modules/fuel-library/deployment/puppet --noop /tmp/modules/fuel-library/deployment/puppet/osnailyfacter/examples/site.pp ; echo Exit code: $? ; echo ) &> /tmp/modules/puppet-apply.${YAML}.log
+    if grep -q 'Exit code: 0' /tmp/modules/puppet-apply.${YAML}.log ; then
+        printf "%-10s %s\n" "OK" "/tmp/modules/puppet-apply.${YAML}.log"
+    else
+        printf "%-10s %s\n" "FAILED" "/tmp/modules/puppet-apply.${YAML}.log"
+    fi
+done
