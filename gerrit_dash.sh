80c7c62ca886e11d6b55c24dfcf94ec13166630a
Aleksandr Didenko <adidenko@mirantis.com>
2014-06-13 19:31:17 +0300

    Fixed dash-creator

--- gerrit_dash.sh
+++ gerrit_dash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl -s https://raw.githubusercontent.com/sdague/gerrit-dash-creator/master/gerrit-dash-creator > /tmp/gerrit-dash-creator 
+curl -s https://raw.githubusercontent.com/stackforge/gerrit-dash-creator/master/gerrit-dash-creator > /tmp/gerrit-dash-creator 
 curl -s https://raw.githubusercontent.com/angdraug/gerrit-dash-creator/fuel-dashboard/dashboards/fuel.dash > /tmp/fuel.dash
 python /tmp/gerrit-dash-creator /tmp/fuel.dash
