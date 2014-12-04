6f6b8037e1684a5fa0051d7ad604eeff257d2e9b
Aleksandr Didenko <adidenko@mirantis.com>
2014-12-04 11:52:36 +0200

    Added check functionality to deploy-reset-test.sh
    deploy-reset-test.sh now can check its own log for errors in
    deployments.

--- deploy-reset-test.sh
+++ deploy-reset-test.sh
@@ -1,9 +1,70 @@
 #!/bin/bash
 # 5 controllers, 1 compute, HA
-num=6
+controllers=5
+computes=1
 env=1
-while true; do
 
+if [[ "$1" != "" ]] ; then
+  if ! [[ -f "$1" ]] ; then
+    echo "$1 file not found"
+    exit 1
+  fi
+  echo
+  deployments=`grep 'Deployment complete' $1  | wc -l`
+  echo "Deployments completed: $deployments"
+  res_enabled=`grep -A $controllers Resource: $1 | grep Enabled | wc -l`
+  res_total=`grep Resource: $1 | wc -l`
+
+  echo
+  echo "==== Constraints ===="
+  echo Resources: $res_total
+  echo "Enabled on nodes:  $res_enabled"
+  res_should_be_enabled=$(( res_total * controllers ))
+  echo "Should be enabled: $res_should_be_enabled"
+  if [[ $res_enabled -eq $res_should_be_enabled ]] ; then
+    echo "Result: OK"
+  else
+    echo "Result: Error. Please check $1 log for details"
+  fi
+  echo
+
+  echo "==== RabbitMQ ===="
+  echo
+
+  echo "Checking cluster nodes"
+  for cid in `fuel node | grep controller | awk '{print $1}'` ; do
+    cnum=`awk '/{nodes,\[/{flag=1}/{running_nodes/{flag=0}flag' $1 | grep -o node-$cid | wc -l`
+    if [[ $cnum -eq $deployments ]] ; then
+      echo "node-$cid: OK ($cnum)"
+    else
+      echo "node-$cid: ERROR ($cnum, but should be $deployments)"
+    fi
+  done
+
+  echo
+  echo "Checking running nodes"
+  for cid in `fuel node | grep controller | awk '{print $1}'` ; do
+    cnum=`awk '/running_nodes,\[/{flag=1}/{cluster_name,/{flag=0}flag' $1 | grep -o node-$cid | wc -l`
+    if [[ $cnum -eq $deployments ]] ; then
+      echo "node-$cid: OK ($cnum)"
+    else
+      echo "node-$cid: ERROR ($cnum, but should be $deployments)"
+    fi
+  done
+
+  echo
+  if grep partitions $1 | grep -v 'partitions,\[\]' ; then
+    echo "ERROR: Partitions found"
+  else
+    echo "Partitions OK"
+  fi
+  echo
+  exit 0
+fi
+
+num=$(( controllers + computes ))
+# Now lets deploy openstack forever :-)
+while true; do
   # Wait for nodes to discover
   discovered=0
   while [[ $discovered -lt $num ]] ; do
