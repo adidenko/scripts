fdcd88937bac6c4a5a08c1d886f796d6751aaada
Aleksandr Didenko <adidenko@mirantis.com>
2017-01-27 13:54:12 +0100

    Add some scripts for k8s debugging

---
+++ k8s/netck.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function get_ns(){
+  awk '/^nameserver/{print $2}' /etc/resolv.conf
+}
+
+function test_tcp(){
+  echo -n "Checking tcp to $1:$2 ... "
+  timeout $3 bash -c "cat < /dev/null > /dev/tcp/$1/$2 && echo OK" || echo ERR
+}
+
+function test_dns(){
+  nslookup $2 $1
+}
+
+dns=$(get_ns)
+while : ; do
+  test_tcp $dns 53 1
+  test_dns $dns nginxsvc
+  test_tcp nginxsvc 80 1
+done
